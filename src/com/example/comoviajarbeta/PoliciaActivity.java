5f90fe0df029f0eb5f7bb49e6a13a6eb19d1109e
Gonzalo Besil <gonzalobesil@gmail.com>
2013-06-03 21:00:39 -0300

    Reportar policia

--- src/com/example/comoviajarbeta/PoliciaActivity.java
+++ src/com/example/comoviajarbeta/PoliciaActivity.java
@@ -2,9 +2,13 @@ package com.example.comoviajarbeta;
 import com.example.comoviajarbeta.dao.*;
 
 import com.example.comoviajarbeta.data.*;
+import com.example.comoviajarbeta.utilidades.ReportarFunciones;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 import android.app.Activity;
@@ -18,17 +22,27 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PoliciaActivity extends Activity {
 	// DAO
 	private RutaDataSource dataSource;
+	// JSON Response node names
+	private static String KEY_SUCCESS = "success";
+	private static String KEY_ERROR = "error";
+	private static String KEY_ERROR_MSG = "error_msg";
+	private static String KEY_UID = "uid";
+	private static String KEY_NAME = "name";
+	private static String KEY_EMAIL = "email";
+	private static String KEY_CREATED_AT = "created_at";
 	// GPSTracker class
 	GPSTracker gps;
 	ListView list;
 	CustomAdapterRuta adapter;
 	public  PoliciaActivity CustomListView = null;
 	Button btnEnviar;
+	TextView reportarErrorMsg;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +61,14 @@ public class PoliciaActivity extends Activity {
         
     	// Importar los elementos contenidos en el layout
         btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        reportarErrorMsg = (TextView) findViewById(R.id.reportar_error);
 		
      // Login button Click Event	   			
 		btnEnviar.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View view) {
 				
-				
+				enviarAlerta();
 				
 			}
 		});	
@@ -65,23 +80,37 @@ public class PoliciaActivity extends Activity {
 
 	public void enviarAlerta()
 	{
-
-
 		gps = new GPSTracker(PoliciaActivity.this);
 
 		// check if GPS enabled		
 		if(gps.canGetLocation()){
 
-			double latitude = gps.getLatitude();
-			double longitude = gps.getLongitude();
+			String latitud = String.valueOf(gps.getLatitude());
+			String longitud = String.valueOf(gps.getLongitude());
 
-			Location location = new Location("");
-			location.setLatitude(latitude);
-			location.setLongitude(longitude);
+			//Location location = new Location("");
+			//location.setLatitude(latitude);
+			//location.setLongitude(longitude);
 
-				
-					
-
+			ReportarFunciones reportarFunciones = new ReportarFunciones();
+			JSONObject json = reportarFunciones.reportar(latitud, longitud, "1");
+			
+			// check for login response
+			try {
+				if (json.getString(KEY_SUCCESS) != null) {
+					reportarErrorMsg.setText("");
+					String res = json.getString(KEY_SUCCESS); 
+					if(Integer.parseInt(res) == 1){
+						reportarErrorMsg.setText("Se reportó con exito");
+					}else{
+						// Error in registration
+						reportarErrorMsg.setText("Error al reportar");
+					}
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		
 			//					        	// \n is for new line
 			//					        	Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude + "\n"+ "distancia: " + (location.distanceTo(dest)/1000), Toast.LENGTH_LONG).show();	
 		}else{
@@ -89,10 +118,10 @@ public class PoliciaActivity extends Activity {
 			// GPS or Network is not enabled
 			// Ask user to enable GPS/network in settings
 			gps.showSettingsAlert();
+			
 		}
-	}
 
-    
+	}
     
    
    
---
+++ src/com/example/comoviajarbeta/utilidades/ReportarFunciones.java
@@ -0,0 +1,47 @@
+
+package com.example.comoviajarbeta.utilidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import android.content.Context;
+
+public class ReportarFunciones {
+	
+	private JSONParser jsonParser;
+	
+	private static String loginURL = "http://10.0.2.2/ah_login_api/";
+	private static String registrarAlertaURL = "http://www.comoviajar.com.uy/androide/guardarAlerta.php";
+	
+	private static String login_tag = "login";
+	private static String register_tag = "register";
+	
+	// constructor
+	public ReportarFunciones(){
+		jsonParser = new JSONParser();
+	}
+	
+	/**
+	 * function make Login Request
+	 * @param email
+	 * @param password
+	 * */
+	public JSONObject reportar(String latitud, String longitud, String tipoAlerta){
+		// Building Parameters
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("latitud", latitud));
+		params.add(new BasicNameValuePair("longitud", longitud));
+		params.add(new BasicNameValuePair("tipoAlerta", tipoAlerta));
+		JSONObject json = jsonParser.getJSONFromUrl(registrarAlertaURL, params);
+		// return json
+		// Log.e("JSON", json.toString());
+		return json;
+	}
+	
+
+	
+}
